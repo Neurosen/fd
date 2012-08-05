59c2dc0ad1bfa1269ce7b155699edb05e65d4e16
Nick <4mzrus@gmail.com>
2012-08-05 23:13:46 +0600

    Update fd.js

--- fd.js
+++ fd.js
@@ -1,26 +1,26 @@
 var fd = { //flash detector
+
     isAvailable : function() {//checking flash for browsers
-        if (typeof navigator.plugins['Shockwave Flash'] != 'undefined') { 
+    
+        if ( typeof navigator.plugins['Shockwave Flash'] !== 'undefined' ) { 
             return true;
-        } else {
-            if (window.ActiveXObject) { //checking flash for ie
-                try {
-                    new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-                    return true;
-                } catch (e) {
-                    return false;
-                };
-            } else {
+        };
+        if (window.ActiveXObject) { //checking flash for ie
+            try {
+                new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+                return true;
+            } catch (e) {
                 return false;
             };
-        };
+        return false;
     },
     
-    getVersion:function(){
+    getVersion : function(){
+        
         var version=null;
-        if (typeof navigator.plugins['Shockwave Flash'] != 'undefined') {
-            var description=navigator.plugins['Shockwave Flash'].description;
-            version=description.substring(16,description.length);
+        if ( typeof navigator.plugins['Shockwave Flash'] !== 'undefined' ) {
+            var description = navigator.plugins['Shockwave Flash'].description;
+            version = description.substring(16, description.length);
         }
         return version;
     }
