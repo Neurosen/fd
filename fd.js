dc4a18b24565bc2e53f13164b56ac90e313ff251
Nick <4mzrus@gmail.com>
2012-08-21 11:16:36 +0600

    A few code style fixes.
    navigator.plugins is now available through closure.

--- fd.js
+++ fd.js
@@ -1,33 +1,21 @@
-fd = { //flash detector
-
-    isAvailable : function() {//checking flash for browsers
+(function(pl){//pl = navigator.plugins
+        fd = { //flash detector
     
-        if ( typeof navigator.plugins['Shockwave Flash'] !== 'undefined' ) { 
-            return true;
-        }
+        isAvailable : function() {//checking flash for browsers
         
-        if (window.ActiveXObject) { //checking flash for ie
-            try {
-                new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+            if ( typeof pl['Shockwave Flash'] !== 'undefined' ) { 
                 return true;
-            } catch (e) {
-                return false;
             }
-        }
             
-        return false;
-    },
-    
-    getVersion : function() {
-        
-        var version = false,
-            description;
-            
-        if ( typeof navigator.plugins['Shockwave Flash'] !== 'undefined' ) {
-            description = navigator.plugins['Shockwave Flash'].description;
-            version = description.substring(16, description.length);
+            if (window.ActiveXObject) { //checking flash for ie
+                try {
+                    new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+                    return true;
+                } catch (e) {
+                    return false;
+                }
+            }
+                
+            return false;
         }
-        
-        return version;
-    }
-};
+})(navigator.plugins);
